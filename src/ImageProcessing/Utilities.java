fabffeb30410e0d6ca2bb28ae0c3dcc816c65d39
MoadBens <benslimanemoad@gmail.com>
2022-03-30 22:31:27 +0200

    TemplateMatching function added

--- src/ImageProcessing/Utilities.java
+++ src/ImageProcessing/Utilities.java
@@ -4,11 +4,20 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfDMatch;
 import org.opencv.core.Scalar;
+import org.opencv.features2d.DescriptorExtractor;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.FeatureDetector;
+import org.opencv.features2d.Features2d;
 import org.opencv.highgui.HighGui;
+import org.opencv.highgui.Highgui;
 import org.opencv.core.MatOfInt4;
+import org.opencv.core.MatOfKeyPoint;
 import org.opencv.core.MatOfPoint;
+import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
+import org.opencv.core.Rect;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
@@ -186,6 +195,72 @@ public class Utilities {
 			return contours;
 		
 		}
-	
+		
+		public static void TemplateMatching(String source1,String source2) {
+			/////////////////////////////// TEMPLATE MATCHING //////////////////////////////////////////////
+			MatOfPoint2f matOfPoint2f= new MatOfPoint2f();
+			   Mat m = readImage(source1);
+			   float[] radius= new float[1];
+			   Point center= new Point();
+			   List<MatOfPoint> contours = detectContours(m);
+			   for (int c=0; c<contours.size();c++) {
+				   MatOfPoint contour=contours.get(c);
+				   double contourArea=Imgproc.contourArea(contour);
+				   matOfPoint2f.fromList(contour.toList());
+				   Imgproc.minEnclosingCircle(matOfPoint2f, center, radius);
+				   if((contourArea/(Math.PI*radius[0]*radius[0]))>=0.8) {
+					   Core.circle(m, center, (int)radius[0],new Scalar(0,255,0),2);
+					   Rect rect = Imgproc.boundingRect(contour);
+					   Core.rectangle(m, new Point(rect.x,rect.y),new Point(rect.x+rect.width,rect.y+rect.height), new Scalar(0,255,0), 2);
+					   Mat tmp=m.submat(rect.y,rect.y+rect.height,rect.x,rect.x+rect.width);
+					   Mat panneau=Mat.zeros(tmp.size(),tmp.type());
+					   tmp.copyTo(panneau);
+					   imShow("Panneau",panneau);
+				   }
+			   }
+			   
+			   //////////////////////////// MISE A L'ECHELLE //////////////////////////////////////////////////
+			   String objectfile=source2;
+			   Mat object=readImage(source2);
+			   Mat sroadSign= HighGui.imread(objectfile);              
+			   Mat sObject = new Mat();
+			   Imgproc.resize(object,sObject,sroadSign.size());
+			   Mat grayObject = new Mat(sObject.rows(),sObject.cols(),sObject.type());
+			   Imgproc.cvtColor(sObject,grayObject,Imgproc.COLOR_BGRA2GRAY);
+			   Core.normalize(grayObject,grayObject,0,255,Core.NORM_MINMAX);
+			   Mat graySign = new Mat(sroadSign.rows(),sroadSign.cols(),sroadSign.type());
+			   Imgproc.cvtColor(sroadSign,graySign,Imgproc.COLOR_BGRA2GRAY);
+			   Core.normalize(graySign,graySign,0,255,Core.NORM_MINMAX);
+			   imShow("test",grayObject);
+			   
+			   //////////////////// EXTRACTION DES CARACTERISTIQUES ////////////////////////////////////
+			   
+			   
+			   FeatureDetector orbDetector = FeatureDetector.create(FeatureDetector.ORB);             
+			   DescriptorExtractor orbExtractor = DescriptorExtractor.create(DescriptorExtractor.ORB);
+			   
+			   MatOfKeyPoint objectKeypoints = new MatOfKeyPoint();
+			   orbDetector.detect(grayObject,objectKeypoints);
+			   
+			   MatOfKeyPoint signKeypoints = new MatOfKeyPoint();
+			   orbDetector.detect(graySign,signKeypoints);
+			   
+			   Mat objectDescriptor= new Mat(object.rows(),object.cols(),object.type());
+			   orbExtractor.compute(grayObject, objectKeypoints,objectDescriptor);
+			   
+			   Mat signDescriptor = new Mat(sroadSign.rows(),sroadSign.cols(),sroadSign.type());
+			   orbExtractor.compute(graySign, signKeypoints,signDescriptor);
+			   
+			   ///////////////////////             MATCHING ////////////////////////////////
+			   MatOfDMatch matchs= new MatOfDMatch();
+			   DescriptorMatcher matcher = DescriptorMatcher.create(DescriptorMatcher.BRUTEFORCE);
+			   matcher.match(objectDescriptor, signDescriptor,matchs);
+			   System.out.println(matchs.dump());
+			   Mat matchedImage = new Mat(sroadSign.rows(),sroadSign.cols()*2,sroadSign.type());
+			   Features2d.drawMatches(sObject,objectKeypoints,sroadSign,signKeypoints,matchs,matchedImage);
+			   
+			   
+			   
+		}
 
 }
