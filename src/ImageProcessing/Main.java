f1aa8f3a7d05d1318aa876e300146b1a0ab1446e
OriginalPou <chaari.mahdi@yahoo.fr>
2022-04-16 20:46:44 +0100

    made changed to the object detection algorithm

--- src/ImageProcessing/Main.java
+++ src/ImageProcessing/Main.java
@@ -1,6 +1,11 @@
 package ImageProcessing;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Vector;
 
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
@@ -11,7 +16,9 @@ import org.opencv.core.MatOfPoint;
 import org.opencv.core.Point;
 import org.opencv.core.Scalar;
 import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
+import org.opencv.photo.Photo;
 
 
 /*
@@ -22,29 +29,39 @@ import org.opencv.imgproc.Imgproc;
 
 public class Main {
     public static void main( String[] args ) {
-      System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
-      Mat testFile = Utilities.readImage("Images/p5.jpg");
-      Utilities.imShow("Original Image", testFile);
-      Mat hsvimage=Utilities.RGB2HSV(testFile);
-      List<MatOfPoint> listeContours = Utilities.detectContours(hsvimage);
-      Mat objetrond = null;
-      for (MatOfPoint contour:  listeContours ){
-			objetrond=Utilities.DetectForm(testFile,contour);
-			if (objetrond!= null) {
-				Utilities.imShow("contour rond", objetrond);
-				break;
-			}
+    	System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	    File imageFolder = new File("Images/data/detected_images/");
+	    File[] listOfImages = imageFolder.listFiles();
+	    Arrays.sort(listOfImages);
+	    for (int i = 0; i < listOfImages.length; i++) {
+	    	if (listOfImages[i].isFile()) {
+	    		int nbOfObject =0;
+	    		//System.out.println("File " + listOfImages[i].getName());
+	    		String filename = listOfImages[i].getName();
+		    	Mat testFile = Utilities.readImage("Images/data/detected_images/"+filename);
+		    	//Mat rescaled_img = Utilities.rescale(testFile);
+		    	Mat rescaled_img = testFile;
+		    	//Utilities.imShow("original Image", rescaled_img);
+		    	    
+		    	List<MatOfPoint> listeContours = Utilities.detectContoursImproved(rescaled_img);
+		    	Mat objetrond = null;
+		    	for (MatOfPoint contour:  listeContours ){
+		    		objetrond=Utilities.DetectForm(rescaled_img,contour);
+		    	    if (objetrond!= null) {
+		    	    	nbOfObject++;
+		    	    	String sign = "Images/data/detected_images/signs/"+filename.substring(0,filename.length()-4)+"_"+Integer.toString(nbOfObject); 
+		    	    	//System.out.println(sign);
+		    	    	//Imgcodecs.imwrite(sign+".png", objetrond); 
+		    	    	//Utilities.imShow(listOfImages[i].getName(), objetrond);
+		    	    	//System.out.println(listOfImages[i].getName());
+		    		}
+		    	}
+		    	if (nbOfObject==0) {
+		    			System.out.println(filename);
+		    	}
+	    	}
       }
-      Mat sign = Utilities.readImage("Images/ref90.jpg");
-      objetrond= Utilities.scale(sign, objetrond);
-      Mat result = new Mat();
-      Core.bitwise_or(objetrond, sign, result);
-      Utilities.imShow("result", result);
-
-      
-      //Utilities.streamVideo("Videos/video1.mp4");
-      
-      
+      //Mat testFile = Utilities.readImage("Images/data/detected_images/00006.png");
       
    }
 }
