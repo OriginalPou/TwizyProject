c33a2a3ea9199663daa7c2e2f2fe67cc441c1f3e
Ali <ali.chouchene@insat.univ-carthage.tn>
2022-04-22 17:45:52 +0100

    100%accuracy

--- src/ImageProcessing/Main.java
+++ src/ImageProcessing/Main.java
@@ -1,6 +1,7 @@
 package ImageProcessing;
 
 import java.util.List;
+import java.util.Vector;
 
 import org.opencv.core.Core;
 import org.opencv.core.CvType;
@@ -22,25 +23,49 @@ import org.opencv.imgproc.Imgproc;
 
 public class Main {
     public static void main( String[] args ) {
+    	
+      
       System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+      Vector<Mat> panels= Utilities.SignPanels();
+      
+
       /*Mat img = Utilities.readImage("Images/ref90.jpg");
       Utilities.GreyMode(img);
       Utilities.BGRMode(img);
       Mat mat = Mat.eye(3, 3, CvType.CV_8UC1);
       System.out.println("mat = " + mat.dump());
-      */Mat testFile = Utilities.readImage("Images/p1.jpg");
-      HighGui.imshow("110km/h", testFile);
+      */
+      Mat testFile = Utilities.readImage("Images/p12.png");
+      //Mat ref = Utilities.readImage("Images/ref70.jpg");
+      Utilities.imShow("test file", testFile);
       Mat hsvimage=Utilities.RGB2HSV(testFile);
-      HighGui.imshow("hsv 110km/h", hsvimage);
-      List<MatOfPoint> listeContours = Utilities.detectContours(hsvimage);
-      Mat objetrond = null;
-      for (MatOfPoint contour:  listeContours ){
-			objetrond=Utilities.DetectForm(testFile,contour);
-			if (objetrond!= null)
-				Utilities.imShow("contour rond", objetrond);
-      }
+      //HighGui.imshow("hsv 110km/h", hsvimage);
+     List<MatOfPoint> listeContours = Utilities.detectContours(hsvimage);
+     Mat objetrond = null;
+		
+		  for (MatOfPoint contour: listeContours ){
+		  objetrond=Utilities.DetectForm(testFile,contour); if (objetrond!= null) {
+		  Utilities.imShow("contour rond", objetrond);
+		  Utilities.Match(objetrond,panels); }
+		  
+		  
+		  }
+		 
+      Utilities.Match(testFile,panels);
+      //HighGui.waitKey(0);
+      //float x=Utilities.Matching(objetrond,ref);
+      //System.out.println(x);
+      //Utilities.imShow("contour rond", objetrond);
+      //Mat image = Utilities.readImage("Images/ref90.jpg");
+      //Utilities.imShow("pannel 90", image);
+      //Mat BW=Utilities.turnBW(image);
+      //Utilities.imShow("pannel 90", BW);
+      //Mat binary=Utilities.turnBinary(BW);
+     // System.out.println( "mat = \n" + binary);
+      //float result=Utilities.Matching2(ref, ref);
+     // System.out.println( "match = \n" + result);
+
       
-      HighGui.waitKey(0);
       
    }
 }
--- src/ImageProcessing/Utilities.java
+++ src/ImageProcessing/Utilities.java
@@ -4,14 +4,19 @@ import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfByte;
+import org.opencv.core.MatOfDMatch;
 import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.features2d.DescriptorMatcher;
+import org.opencv.features2d.Features2d;
 import org.opencv.highgui.HighGui;
+import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.core.MatOfInt4;
+import org.opencv.core.MatOfKeyPoint;
 import org.opencv.core.MatOfPoint;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
 import org.opencv.core.Rect;
-import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 
 import java.awt.image.BufferedImage;
@@ -172,8 +177,8 @@ public class Utilities {
 		 * @return : List of point of a specific contour
 		 */
 		public static List<MatOfPoint> detectContours(Mat mat) {
-			Mat threshold_img=multipleThreshhold(mat, 6, 170, 110);
-			int thresh =50;
+			Mat threshold_img=multipleThreshhold(mat, 6, 140, 80);
+			int thresh =40;
 			Mat canny_output=new Mat();
 			List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
 			MatOfInt4 hierarchy = new MatOfInt4();
@@ -244,5 +249,221 @@ public class Utilities {
 			return(grayObject);
 		}
 	
+		
+		
+		/*
+		 * @brief : function that compare 2 images 
+		 * @return : the similarity between the 2 images
+		 */
+		public static float MatchingWithDifference(Mat object, Mat sObject) {
+			float similarity=0;
+			int numberOfZeros=0;
+			// gray scale
+			Mat grayObject=turnBW(object);
+			Mat graySign=turnBW(sObject);
+			// resize image
+			grayObject=scale(graySign,grayObject);
+			
+			// total number of pixels
+			int totalNumberOfPixels=0;
+			for(int i=0;i<graySign.width();i++) {
+				for(int j=0;j<graySign.height();j++) {
+					double[] pixelSign=graySign.get(i, j);
+					double[] pixelpanel=grayObject.get(i, j);
+					if( ( (int)pixelSign[0]<80) || ((int)pixelpanel[0]<80) ) {
+					totalNumberOfPixels++;
+					int x=(int)pixelSign[0]-(int)pixelpanel[0];
+					if(Math.abs(x)<10) {
+						numberOfZeros++;
+					}
+					}
+				}
+			}
+				similarity=(float)numberOfZeros/totalNumberOfPixels;
+				return similarity;
+			}
+		
+			
+		
+		/*
+		 * @brief : function one image with all panels 
+		 * @return : shows the panel that match
+		 */
+		
+		public static void Match(Mat object,List<Mat> panels) {
+			float[] results=new float[panels.size()];
+			float max=0;
+			int index=0;
+			for(int i=0;i<panels.size();i++) {
+				results[i]=MatchingWithRGB(object,panels.get(i));
+				//System.out.println(results[i]);
+			}
+			for(int i=0;i<panels.size();i++) {
+			if(results[i]>max) {
+				max=results[i];
+				index=i;
+			}
+			}
+			
+			
+			imShow("pannel detected",panels.get(index));
+		}
+			
+		
+		/*
+		 * @brief : create a vector of images  
+		 * @return : vector of panel images
+		 */
+			
+			public static Vector<Mat> SignPanels(){
+				
+			 int[] panels= {30,50,70,90,110};
+		     Vector<Mat> Signs=new Vector<Mat>();
+		     for(int i=0;i<panels.length;i++) {
+		    	  Mat image = Utilities.readImage("Images/ref"+Integer.toString(panels[i])+".jpg");
+		    	  Signs.add(image);
+		      }
+		     Mat image = Utilities.readImage("Images/refdouble.jpg");
+		     Signs.add(image);
+		      return Signs;
+			}
+			
+		
+		
+		
+		public static Mat turnBinary (Mat image) {
+			Mat binaryObject = new Mat(image.rows(),image.cols(), image.type());
+			for(int i=0;i<image.width();i++) {
+				for(int j=0;j<image.height();j++) {
+					double[] pixel=image.get(i, j);
+					if((int)pixel[0]>=200)
+						binaryObject.put(i, j, 0);
+					else
+						binaryObject.put(i, j, 1);
+					
+				}
+			}
+			
+			return(binaryObject);
+		}
+		
+		public static float MatchingWithPPSimilarity(Mat object, Mat sObject) {
+			float similarity=0;
+			//sObject=scale(object,sObject);
+			// gray scale
+			Mat grayObject=turnBW(object);
+			Mat graySign=turnBW(sObject);
+			grayObject=scale(graySign,grayObject);
+			//graySign=scale(grayObject,graySign);
+		   // imShow("obj", sObject);
+			Mat binarySign=turnBinary(graySign);
+			Mat binaryObject=turnBinary(grayObject);
 
+			// total number of pixels
+			int totalNumberOfPixels=graySign.width()*graySign.height();//=graySign.height()*graySign.height();
+			for(int i=0;i<graySign.width();i++) {
+				for(int j=0;j<graySign.height();j++) {
+					double[] xySign   = binarySign.get(i,j);
+					double[] xyObject = binaryObject.get(i,j);
+					int xy1= (int)xySign[0];
+					int xy2= (int)xyObject[0];
+					//System.out.println( "ij = \n" + (int)xyObject[0] );
+					if(  xy1 == xy2 )
+						similarity++;
+					}
+				}
+			
+				similarity=(float)similarity/totalNumberOfPixels;
+				return similarity;
+			}
+		
+		
+		public static float MatchingWithRGB(Mat object, Mat sObject) {
+			//Increasing the brightness of an image
+			//imShow("before", object);
+		    //object.convertTo(object, -1, 1, -10);
+		    //Imgproc.blur(object, object, new Size(3.0, 3.0));
+			//imShow("after", object);
+
+			Mat img1=scale(sObject,object);
+			Mat img2=sObject;
+			float brightness=getBrightness(img2);
+			//System.out.println("brightness="+brightness);
+			if(brightness<0.15)
+				img2.convertTo(img2, -1, 1, 60);
+			if(brightness>0.8)
+				img2.convertTo(img2, -1, 1, -10);
+			
+		    //imShow("obj", img1);
+			int height=img1.height();
+			int width =img1.width();
+			double diff = 0;
+	         for (int j = 0; j < height; j++) {
+	            for (int i = 0; i < width; i++) {
+	               //Getting the RGB values of a pixel
+	               double pixel1[] = img1.get(i, j);
+	               double r1 = pixel1[0];
+	               double g1 = pixel1[1];
+	               double b1 = pixel1[2];
+	               double pixel2[] = img2.get(i, j);
+	               double r2 = pixel2[0];
+	               double g2 = pixel2[1];
+	               double b2 = pixel2[2];
+	               //sum of differences of RGB values of the two images
+	               double data = Math.abs(r1-r2)+Math.abs(g1-g2)+ Math.abs(b1-b2);
+	               diff = diff+data;
+	            }
+	         }
+	         double avg = diff/(width*height*3);
+	         double percentageOfSimilarity =100- (avg/255)*100;
+	        // System.out.println("similarity: "+percentageOfSimilarity);
+	         return (float)percentageOfSimilarity;
+	      }
+	   
+		
+		
+		public static float getBrightness(Mat image) {
+			
+			double[] color = image.get(image.width()/2, image.height()/2);
+
+			// extract each color component
+			// calc brightness in range 0.0 to 1.0; using SRGB luminance constants
+			float brightness = ((float)color[0] * 0.2126f + (float)color[1] * 0.7152f + (float)color[2] * 0.0722f) / 255;
+
+			return brightness;
+			
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 }
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+			
+			
+
+		
+		
+		
+		
+
+
