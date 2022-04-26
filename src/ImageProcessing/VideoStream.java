17aec6c785e8180e9254a9458e1bf1ffd6dd1231
mahag <mahagaied@gmail.com>
2022-04-26 11:16:11 +0200

    panels added

--- src/ImageProcessing/VideoStream.java
+++ src/ImageProcessing/VideoStream.java
@@ -25,6 +25,10 @@ public class VideoStream {
 	//Mat PanneauAAnalyser = null;
 	ImageIcon image;
 	Image image_90;
+	Image image_70;
+	Image image_50;
+	Image image_30;
+	Image image_110;
 	
 	
 	//constructor
@@ -35,10 +39,13 @@ public class VideoStream {
 		this.initImage();
 		
 	}
-	
-	// TODO: must change the image to an empty image 
+ 
 	public void initImage() throws IOException {
 		this.image_90=ImageIO.read(new File("images/ref90.jpg"));
+		this.image_70=ImageIO.read(new File("images/ref70.jpg"));
+		this.image_50=ImageIO.read(new File("images/ref50.jpg"));
+		this.image_30=ImageIO.read(new File("images/ref30.jpg"));
+		this.image_110=ImageIO.read(new File("images/ref110.jpg"));
 	}
 	
 	//run the video processing algorithm
@@ -50,14 +57,13 @@ public class VideoStream {
 		while (camera.read(frame)) {
 			
 			if (frame_index % speed == 0) {
-				Mat HSV_image=Utilities.RGB2HSV(frame);
+				//Mat HSV_image=Utilities.RGB2HSV(frame);
 				List<MatOfPoint> ListContours= Utilities.detectContoursImproved(frame);
 				
 				Mat round_object = null;
 		
 				for (MatOfPoint contour: ListContours  ){
 					round_object=Utilities.DetectForm(frame,contour);
-					
 					if (round_object!=null){
 						//Utilities.imShow("contour", round_object);
 						//Utilities.Match(round_object,panels);
@@ -71,7 +77,6 @@ public class VideoStream {
 				this.window.panel_video.repaint();
 			
 				frame_index=frame_index+1;
-			
 		}	
 	}
 	
