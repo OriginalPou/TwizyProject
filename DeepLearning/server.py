7bc61796a1086a799edadba40fb3dd794ed8513f
OriginalPou <chaari.mahdi@yahoo.fr>
2022-08-13 21:28:49 +0100

    added images/Dataset to serve as examples and turned back on the functionality of running the project without opencv installed locally

--- DeepLearning/server.py
+++ DeepLearning/server.py
@@ -1,21 +1,30 @@
+''' This script is used to run a yolov5 model trained on traffic sign detection and recognition
+The main idea is to communicate with a Java program through a socket communication point
+The Java program is used to set up the GUI while this script runs inference on the images and video frames
+and sends the inference results to the GUI which displays them to the user
+'''
+
+
 import socket
 import torch
 import time
 import pandas
 
+# this method loads the yolov5 model 
 def load_model () :
 	model = torch.hub.load('ultralytics/yolov5', 'custom', path='best.pt')
 	return(model)
 
+#This method runs inference on the images and video frames 
 def runInference (model, image_path) :
 	# run inference
 	results = model(image_path)
 	# return results (pandas dataframe)
 	return(results.pandas().xyxy[0])
 
-
+#This method prepares the data to be sent to the Java program using a clear protocol
 def prepareData(results):
-	df=results #results is a pandas dataframe
+	df=results 			#results is a pandas dataframe
 	result=""
 	for index in df.index:
 		if df.loc[index,"confidence"]>0.5:
@@ -27,12 +36,16 @@ def prepareData(results):
 			result=result+ str(float("{:.2f}".format(df.loc[index,"confidence"])))
 	return (result+"\n")
 
+#
+'''	This method is used to initiate the server using a socket
+	and communicate back and forth with the client
+'''
 def server():
 	model=load_model()
 	print ("model loaded into memory")
 	
-	host = "localhost"   # get local machine name
-	port = 9999  # Make sure it's within the > 1024 $$ <65535 range
+	host = "localhost"   	
+	port = 9999  		
 	 
 	s = socket.socket()
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
